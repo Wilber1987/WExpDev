141f3fd505e4bc268e2973cf89ae534977688eb0
Wilber1987 <wilberj1987@gmail.com>
2020-03-03 17:27:27 -0600

    30-10

--- Scripts/modules/WchartComponets.js
+++ Scripts/modules/WchartComponets.js
@@ -17,15 +17,42 @@ function DrawDoubleGroupChart(Config){
     var Config = new ChartConfig(Config);
     var ChartContainer = GetObj(Config.ContainerName);  
     ChartContainer.className = "WChartContainer";  
-    var SectionTitle = CreateStringNode(`<h3>${Config.Title}</h3>`)
-    var SectionLabels = CreateStringNode(`<section></section>`)
+    var SectionTitle = CreateStringNode(`<h3>${Config.Title}</h3>`);
+    var SectionLabels = document.createElement('section');
+    SectionLabels.className = "SectionLabels"
+    var SectionLabelGroup = document.createElement('section');
+    SectionLabelGroup.className = "SectionLabelGroup";
+    //labels data son las series
     Config.GroupLabelsData.forEach(element => {   
         for (const prop in element) {
-            SectionLabels.append(CreateStringNode(
+            SectionLabels.appendChild(CreateStringNode(
                 `<div>${element[prop]}</div>`
             )); 
         }          
     })    
-    ChartContainer.append(SectionTitle,SectionLabels);
-
+    var SectionBars = document.createElement('section');
+    SectionBars.className="SectionBars";
+        Config.GroupDataset.forEach(elementGroup => {
+        var groupBars = document.createElement("groupBar");
+        groupBars.className = "groupBars";
+        Config.Datasets.forEach(element => {
+            if (element.time == elementGroup.time) {                
+                var Bars = CreateStringNode(`
+                <Bars class="Bars" style="height:${element.cantidad}px">
+                    <label>
+                        ${element.cantidad}
+                    </labe>
+                </Bars>`) 
+                groupBars.appendChild(Bars);                                  
+            }      
+            console.log(groupBars)                      
+        })
+        SectionBars.append(groupBars);
+        SectionLabelGroup.append(CreateStringNode(`       
+            <label class="groupLabels">
+                ${elementGroup.time}
+            </labe>`) 
+        )                  
+    })   
+    ChartContainer.append(SectionTitle,SectionLabels,SectionBars,SectionLabelGroup);
 }
