df0e580fd438a4e723ff82ade55c9a214371cebb
Wilber1987 <wilberj1987@gmail.com>
2020-07-15 16:07:32 -0600

    30-10

--- INCLUSIVE_PROYECT/Scripts/Modules/WChartJSComponent.js
+++ INCLUSIVE_PROYECT/Scripts/Modules/WChartJSComponent.js
@@ -6,16 +6,13 @@ class ChartConfig {
     constructor(Config) {
         this.ContainerName = Config.ContainerName;
         this.Title = Config.Title;
+        this.AttNameEval = Config.AttNameEval;
         this.EvalValue = Config.EvalValue;
-        this.GroupDataset = Config.GroupDataset;//primera agrupacion
         this.AttNameG1 = Config.AttNameG1;
-        this.SecondGroupDataset = Config.SecondGroupDataset;//segunda agrupacion
         this.AttNameG2 = Config.AttNameG2;
-        this.ThreeGroupDataset = Config.ThreeGroupDataset;//triple agrupacion
         this.AttNameG3 = Config.AttNameG3;
         this.GroupLabelsData = Config.GroupLabelsData;//series
-        this.Datasets = Config.Datasets; //datos
-        this.AttNameEval = Config.AttNameEval;
+        this.Datasets = Config.Datasets; //datos        
         this.Colors = Config.Colors;
         this.GroupDataTotals = Config.GroupDataTotals;
         this.ContainerSize = Config.ContainerSize;
